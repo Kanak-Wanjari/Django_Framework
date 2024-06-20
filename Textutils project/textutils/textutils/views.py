e713546c16a3d4c78e2b5f491719a66ad9d92e95
Kanak Wanjari <wanjarikanak@gmail.com>
2024-06-20 13:52:50 +0530

    Commit

--- Textutils project/textutils/textutils/views.py
+++ Textutils project/textutils/textutils/views.py
@@ -1,6 +1,22 @@
 #I have created this file
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Home")
+
+def removepunc(request):
+    return HttpResponse("Removepunc")
+
+def capfirst(request):
+    return HttpResponse("CapItalize First")
+
+def newlineremove(request):
+    return HttpResponse("New Line Removed")
+
+def spaceremove(request):
+    return HttpResponse("Space removed")
+
+def charcount(request):
+    return HttpResponse("Character Count")
