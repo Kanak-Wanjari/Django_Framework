4caa984d6e7e8abaeb97cb8e0af6b7e5527f8b2f
Kanak Wanjari <wanjarikanak@gmail.com>
2024-07-30 16:28:23 +0530

    Commit

--- Textutils project/textutils/textutils/views.py
+++ Textutils project/textutils/textutils/views.py
@@ -10,7 +10,8 @@ def analyze(request):
     djtext = request.GET.get('text', 'default')
     removepunc = request.GET.get('removepunc','off')
     fullcaps = request.GET.get('fullcaps','off')
-    newlineremover =request.GET.get('newlineremover','off')
+    newlineremover = request.GET.get('newlineremover','off')
+    extraspaceremover = request.GET.get('extraspaceremover','off')
     if removepunc == 'on':
         punctuations = '''!()-[]{;:}'"\,<>./?@#$%^&*_~'''
         analyzed=""
@@ -31,6 +32,13 @@ def analyze(request):
             if char != "\n":
                 analyzed = analyzed + char
         params = {'purpose':'New Line Removed' , 'analyzed_text':analyzed}
+        return render(request, 'analyze.html', params)
+    elif(extraspaceremover == 'on'):
+        analyzed=""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose':'Extra Space Removed' , 'analyzed_text':analyzed}
         return render(request, 'analyze.html', params)
     else:
         return HttpResponse("Error")
